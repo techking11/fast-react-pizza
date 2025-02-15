716c5f9b8d51f5db4530f5da6494def7c0235b9a
techking11 <nma1132000@gmail.com>
2025-02-15 13:20:37 +0700

    add menu feature

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,21 @@
+import { getMenu } from '@/services/apiRestaurant';
+import { useLoaderData } from 'react-router-dom';
+import MenuItem from '@/features/menu/MenuItem';
+
+function Menu() {
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
